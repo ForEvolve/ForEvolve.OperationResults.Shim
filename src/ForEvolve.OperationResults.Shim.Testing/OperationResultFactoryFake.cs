f2b56a1a1a4a09eb4c4436d6d19753f6b4ec4e7d
Carl-Hugo Marcotte <carlhugo@gmail.com>
2022-12-19 19:42:17 -0500

    Add testing utilities

---
+++ src/ForEvolve.OperationResults.Shim.Testing/OperationResultFactoryFake.cs
@@ -0,0 +1,39 @@
+﻿using ForEvolve.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.Internal;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ForEvolve.OperationResults
+{
+    public class OperationResultFactoryFake : IOperationResultFactory
+    {
+        public IOperationResultFactory Instance { get; }
+
+        public OperationResultFactoryFake()
+        {
+            var services = new ServiceCollection();
+            services
+                .AddSingleton<IHostingEnvironment, HostingEnvironment>()
+                .AddForEvolveOperationResults();
+
+            Instance = services
+                .BuildServiceProvider()
+                .GetService<IOperationResultFactory>();
+        }
+
+        public IOperationResult Create()
+        {
+            return Instance.Create();
+        }
+
+        public IOperationResult<TResult> Create<TResult>()
+        {
+            return Instance.Create<TResult>();
+        }
+
+        public IOperationResult<TResult> Create<TResult>(TResult result)
+        {
+            return Instance.Create(result);
+        }
+    }
+}
