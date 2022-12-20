f823e91f6443d06ab5dd4e813919534a1f57961e
Carl-Hugo Marcotte <carlhugo@gmail.com>
2022-12-19 20:02:38 -0500

    Add AssertEqual extension method

---
+++ src/ForEvolve.OperationResults.Shim.Testing/ErrorAssertExtensions.cs
@@ -0,0 +1,26 @@
+﻿using ForEvolve.Contracts.Errors;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ForEvolve.Api.Contracts.Errors
+{
+    public static class ErrorAssertExtensions
+    {
+        public static JsonSerializerSettings JsonSerializerSettings => new JsonSerializerSettings
+        {
+            NullValueHandling = NullValueHandling.Ignore
+        };
+
+        public static void AssertEqual(this Error expected, Error actual)
+        {
+            var expectedJson = JsonConvert.SerializeObject(expected, JsonSerializerSettings);
+            var actualJson = JsonConvert.SerializeObject(actual, JsonSerializerSettings);
+            Assert.Equal(expectedJson, actualJson);
+        }
+    }
+}
