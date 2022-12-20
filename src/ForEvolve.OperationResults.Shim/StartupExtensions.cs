659bd3e86b91525f88747a6ae82c5cec055cf611
Carl-Hugo Marcotte <carlhugo@gmail.com>
2022-12-19 19:01:48 -0500

    Add startup extensions

---
+++ src/ForEvolve.OperationResults.Shim/StartupExtensions.cs
@@ -0,0 +1,47 @@
+using ForEvolve.AspNetCore;
+using ForEvolve.AspNetCore.ErrorFactory.Implementations;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ForEvolveErrorFactoryStartupExtensions
+    {
+        /// <summary>
+        /// Adds the <c>IErrorFactory</c> services to the specified <c>Microsoft.Extensions.DependencyInjection.IServiceCollection</c>.
+        /// </summary>
+        /// <param name="services">The <c>Microsoft.Extensions.DependencyInjection.IServiceCollection</c> to add the service to.</param>
+        /// <returns>A reference to this instance after the operation has completed.</returns>
+        public static IServiceCollection AddForEvolveErrorFactory(this IServiceCollection services)
+        {
+            services.TryAddSingleton<IErrorFromOperationResultFactory, DefaultErrorFromOperationResultFactory>();
+            services.TryAddSingleton<IErrorFromSerializableErrorFactory, DefaultErrorFromSerializableErrorFactory>();
+            services.TryAddSingleton<IErrorFromIdentityErrorFactory, DefaultErrorFromIdentityErrorFactory>();
+            services.TryAddSingleton<IErrorFromExceptionFactory, DefaultErrorFromExceptionFactory>();
+            services.TryAddSingleton<IErrorFromDictionaryFactory, DefaultErrorFromDictionaryFactory>();
+            services.TryAddSingleton<IErrorFromKeyValuePairFactory, DefaultErrorFromKeyValuePairFactory>();
+            services.TryAddSingleton<IErrorFromRawValuesFactory, DefaultErrorFromRawValuesFactory>();
+            services.TryAddSingleton<IErrorFactory, DefaultErrorFactory>();
+
+            DefaultErrorFactory.Current = services
+                .BuildServiceProvider()
+                .GetService<IErrorFactory>();
+
+            return services;
+        }
+    }
+
+    public static class ForEvolveOperationResultsStartupExtensions
+    {
+        /// <summary>
+        /// Adds the <c>IOperationResultFactory</c> services to the specified <c>Microsoft.Extensions.DependencyInjection.IServiceCollection</c>.
+        /// </summary>
+        /// <param name="services">The <c>Microsoft.Extensions.DependencyInjection.IServiceCollection</c> to add the service to.</param>
+        /// <returns>A reference to this instance after the operation has completed.</returns>
+        public static IServiceCollection AddForEvolveOperationResults(this IServiceCollection services)
+        {
+            services.TryAddSingleton<IOperationResultFactory, DefaultOperationResultFactory>();
+            services.AddForEvolveErrorFactory();
+            return services;
+        }
+    }
+}
